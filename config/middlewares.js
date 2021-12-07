05f56bd96d824ffcbfe95c865e7b49ab14a04390
MAlngaawy <m.alngaawy@gmail.com>
2021-12-07 16:13:27 +0200

    another try

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,19 +9,31 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
   // ...
+  // {
+  //   name: 'strapi::security',
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         'connect-src': ["'self'", 'https:'],
+  //         'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+  //         'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+  //         upgradeInsecureRequests: null,
+  //       },
+  //     },
+  //   },
+  // },
+
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
-        useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
         },
       },
     },
   },
+
   // ...
 ];
